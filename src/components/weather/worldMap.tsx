4c9c90b982ddcf3b93fd03ffbbfdf56d7b1728df
debugme <debugme@hotmail.com>
2022-11-30 13:47:41 +0000

    commit 25

--- src/components/weather/worldMap.tsx
+++ src/components/weather/worldMap.tsx
@@ -10,8 +10,8 @@ export const WorldMap: FC<WorldMapProps> = (props) => {
   const title = `${city}, ${country}`
 
   return (
-    <span className="h-56 md:h-60 lg:h-72 bg-gradient-to-r from-primary-300 to-primary-400 w-full mt-4 flex justify-center  items-center text-secondary-700 px-8 py-2 rounded-t-lg">
-      <World country={country} latitude={latitude} longitude={longitude} />
+    <span className="h-56 md:h-60 lg:h-72 bg-secondary-400 w-full mt-4 flex justify-center items-center text-secondary-700 px-8 py-2 rounded-t-lg">
+      <span><World latitude={latitude} longitude={longitude} /></span>
       <h4 className="text-center block text-sm w-16">{title}</h4>
     </span>
   )
--- src/components/world/index.tsx
+++ src/components/world/index.tsx
@@ -4,13 +4,12 @@ import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps
 import geoUrl from "./map.json"
 
 export type WorldProps = {
-  country: string
   latitude: number
   longitude: number
 }
 
 export const World: FC<WorldProps> = (props) => {
-  const { country, latitude, longitude } = props
+  const { latitude, longitude } = props
 
   return (
     <ComposableMap className="w-full">
@@ -18,13 +17,13 @@ export const World: FC<WorldProps> = (props) => {
         {({ geographies }) =>
           geographies.map((geo) => {
             return (
-              <Geography key={geo.rsmKey} geography={geo} fill="#ca8a04" />
+              <Geography key={geo.rsmKey} geography={geo} fill="#e2e8f0" />
             )
           })
         }
       </Geographies>
       <Marker coordinates={[longitude, latitude]}>
-        <circle r={16} fill="red" stroke="white" strokeWidth={4} />
+        <circle r={12} fill="red" stroke="white" strokeWidth={3} />
       </Marker>
     </ComposableMap>
   )
