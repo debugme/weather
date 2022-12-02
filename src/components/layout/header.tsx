83d76b996c5988b0f5a8ea1850b3bfc188988a3a
debugme <debugme@hotmail.com>
2022-12-02 17:13:57 +0000

    commit 42

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
       <Menu className="h-9 text-secondary-400" />
       <span className="flex items-center justify-center cursor-pointer gap-2" onClick={onClick} title="Click to toggle theme">
         <h3 className="text-transparent bg-clip-text bg-gradient-to-r from-primary-600 via-primary-400 to-primary-300">Tom Grunge</h3>
-        <Avatar className="w-10 h-10 border rounded-full bg-secondary-700 border-primary-600" />
+        <Avatar className="w-10 h-10 border rounded-full border-secondary-400 fill-secondary-500 bg-secondary-500" />
       </span>
     </header>
   )
