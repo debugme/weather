4bc0262dd0d92d34d5b4969a0c6c2251883c7950
debugme <debugme@hotmail.com>
2022-12-07 17:52:01 +0000

    commit 51

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,29 @@
-import { useState } from "react"
+import { Fragment, useState } from "react"
+import { Toggle } from "../toggle"
 
 import { Avatar } from "./avatar"
 import { Menu } from "./menu"
+
+
+export const Settings = () => {
+  return (
+    <div className="block absolute z-10 bg-secondary-700 top-[72px] bottom-[72px] w-full">
+      <h2 className="text-secondary-300 text-2xl px-8 pt-2">Settings</h2>
+      <section className="flex flex-col px-8 pt-4">
+        <span className="py-3 text-secondary-300 rounded-lg w-fit">Show breakpoints</span>
+        <Toggle isOn={false} onClick={() => { }} />
+      </section>
+      <section className="flex flex-col px-8 pt-4">
+        <span className="py-3 text-secondary-300 rounded-lg w-fit">Toggle theme</span>
+        <Toggle isOn={false} onClick={() => { }} />
+      </section>
+    </div>
+  )
+}
+
+
+
+
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -12,9 +34,12 @@ export const Header = () => {
   const toggleTheme = () => document.body.classList.toggle("tokyo")
 
   return (
-    <header className="flex items-center justify-between px-6 bg-secondary-700 font-cursive">
-      <Menu isOpen={isOpen} onClick={toggleMenu} />
-      {showAvatar && <Avatar onClick={toggleTheme} />}
-    </header>
+    <Fragment>
+      <header className="flex items-center justify-between px-6 bg-secondary-700 font-cursive">
+        <Menu isOpen={isOpen} onClick={toggleMenu} />
+        {showAvatar && <Avatar onClick={toggleTheme} />}
+      </header>
+      { isOpen ? <Settings /> : null }
+    </Fragment>
   )
 }
--- src/components/toggle/index.tsx
+++ src/components/toggle/index.tsx
@@ -6,17 +6,19 @@ export type ToggleProps = {
 }
 
 export const Toggle: FC<ToggleProps> = (props) => {
-  const { isOn, onClick } = props
-
-  const [isEnabled, setEnabled] = useState(isOn)
-  const toggle = () => { 
-    onClick(!isEnabled)
+  const [isEnabled, setEnabled] = useState(props.isOn)
+  
+  const onClick = () => { 
+    props.onClick(!isEnabled)
     setEnabled(isEnabled => !isEnabled) 
   }
-  const alignment = isEnabled ? "right-[3px]" : "left-[3px]"
+
+  const toggleBoxClassList = `relative w-10 border border-secondary-700  h-6 rounded-2xl cursor-pointer ${isEnabled ? "bg-primary-400" : "bg-secondary-400" }`
+  const toggleClassList = `transition-all absolute h-4 w-4 rounded-full bg-secondary-700 top-[3px] ${isEnabled ? "right-[3px]" : "left-[3px]"}`
+
   return (
-    <div className="relative w-10 border border-secondary-700 bg-secondary-400 h-6 rounded-2xl cursor-pointer" onClick={toggle}>
-      <span className={"transition-all absolute h-4 w-4 rounded-full bg-secondary-700 top-[3px] " + alignment} />
+    <div className={toggleBoxClassList} onClick={onClick}>
+      <span className={toggleClassList} />
     </div>
   )
 }
