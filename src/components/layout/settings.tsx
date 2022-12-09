ef546cacec57b55175a27253492d4d1fdb317830
debugme <debugme@hotmail.com>
2022-12-09 00:08:04 +0000

    commit 58

--- src/components/layout/settings.tsx
+++ src/components/layout/settings.tsx
@@ -7,15 +7,25 @@ export const Settings = () => {
 
   return (
     <div className="block absolute z-10 bg-secondary-700 top-[72px] bottom-[72px] w-full">
+      
       <h2 className="text-secondary-300 text-2xl px-8 pt-2">Settings</h2>
-      <section className="flex flex-col px-8 pt-4">
-        <span className="py-3 text-secondary-300 rounded-lg w-fit">Show breakpoints</span>
+      
+      <div className="my-8" />
+      
+      <section className="flex flex-col pb-4 px-4 mx-8 mt-4 rounded-lg bg-secondary-800 w-80">
+        <span className="py-3 text-secondary-300 rounded-lg">Tooling</span>
+        <p className="text-secondary-400 pb-4">Allows you to show or hide what breakpoints are supported</p>
         <Toggle isOn={showBreakpoints} onClick={toggleBreakpoints} />
       </section>
-      <section className="flex flex-col px-8 pt-4">
-        <span className="py-3 text-secondary-300 rounded-lg w-fit">Choose theme</span>
-        <Selector selectedOption={theme} setSelectedOption={setTheme} options={themeList}/>
+
+      <div className="my-8" />
+
+      <section className="flex flex-col pb-4 px-4 mx-8 mt-4 rounded-lg bg-secondary-800 w-80">
+        <span className="py-3 text-secondary-300 rounded-lg">Theming</span>
+        <p className="text-secondary-400 pb-4">Allows you to choose between cool and warm themes</p>
+        <Selector selectedOption={theme} setSelectedOption={setTheme} options={themeList} />
       </section>
+
     </div>
   )
 }
--- src/components/selector/index.tsx
+++ src/components/selector/index.tsx
@@ -19,7 +19,7 @@ export const Selector: FC<SelectorProps> = (props) => {
 
   const itemList = options.map(text => {
     const isActive = (text === selectedOption)
-    const className = `${isActive ? "text-secondary-700 bg-primary-400" : "text-secondary-400 bg-secondary-600"}`
+    const className = `${isActive ? "text-primary-400 bg-secondary-700" : "text-secondary-700"}`
     return (
       <li key={text} className={`py-2 px-3 rounded-xl cursor-pointer ${className}`}>
         <h4 onClick={onClick}>{text}</h4>
@@ -28,7 +28,7 @@ export const Selector: FC<SelectorProps> = (props) => {
   })
 
   return (
-    <ul className="bg-secondary-600 flex max-w-fit py-3 px-3 rounded-2xl gap-4">
+    <ul className="bg-primary-400 flex max-w-fit py-3 px-3 rounded-2xl gap-4">
       {itemList}
     </ul>
   )
