93c4832a1ec7fd74c62fd6886f24ce87750436bf
debugme <debugme@hotmail.com>
2022-12-10 20:00:38 +0000

    commit 62

--- src/components/layout/avatar.tsx
+++ src/components/layout/avatar.tsx
@@ -3,8 +3,8 @@ import { Avatar as AvatarIcon } from "../images"
 export const Avatar = () => {
   return (
     <span className="flex items-center justify-center gap-2">
-      <h3 className="hidden sm:block text-transparent bg-clip-text bg-gradient-to-r from-primary-600 via-primary-400 to-primary-300">Tom Grunge</h3>
-      <AvatarIcon className="w-10 h-10 border rounded-full border-secondary-400 fill-secondary-500 bg-secondary-500" />
+      <h3 className="text-sm hidden sm:block text-transparent bg-clip-text bg-gradient-to-r from-primary-600 via-primary-400 to-primary-300">Tom Grunge</h3>
+      <AvatarIcon className="w-10 h-10 border-none" />
     </span>
   )
 }
