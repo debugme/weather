4a778e8afce27df3c261af273be71a61c68c92fc
debugme <debugme@hotmail.com>
2022-12-11 12:12:46 +0000

    commit 73

--- src/components/toggle/index.tsx
+++ src/components/toggle/index.tsx
@@ -14,7 +14,7 @@ export const Toggle: FC<ToggleProps> = (props) => {
   }
 
   const toggleBoxClassList = `relative w-14 border border-primary-600  h-8 rounded-2xl cursor-pointer ${isEnabled ? "bg-primary-600" : "bg-secondary-600"}`
-  const toggleClassList = `transition-all absolute h-6 w-6 rounded-full top-[3px] ${isEnabled ? "right-[3px] bg-secondary-600" : "left-[3px] bg-secondary-400"}`
+  const toggleClassList = `transition-all absolute h-6 w-6 rounded-full top-[3px] ${isEnabled ? "right-[3px] bg-secondary-600" : "left-[3px] bg-primary-600"}`
 
   return (
     <div className={toggleBoxClassList} onClick={onClick}>
