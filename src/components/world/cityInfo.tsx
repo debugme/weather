478e0bd4f3c4a3d61cedecb4fc7406b6db9662cc
debugme <debugme@hotmail.com>
2022-12-12 09:56:54 +0000

    commit 86

---
+++ src/components/world/cityInfo.tsx
@@ -0,0 +1,24 @@
+import { FC, useEffect, useRef, useState } from "react"
+
+import { getTimeFormatter } from "../../utilities"
+
+export type CityInfoProps = {
+  countryCode: string
+}
+
+export const CityInfo: FC<CityInfoProps> = (props) => {
+  const { countryCode } = props
+
+  const intervalId = useRef<NodeJS.Timeout>()
+  const [localTime, setLocalTime] = useState(getTimeFormatter("GB", "medium").format(Date.now()))
+  const oneSecond = 1000
+
+  useEffect(() => {
+    const callback = () => setLocalTime(getTimeFormatter(countryCode || "GB", "medium").format(new Date()))
+    const cleanUp = () => clearInterval(intervalId.current)
+    intervalId.current = setInterval(callback, oneSecond)
+    return cleanUp
+  }, [countryCode])
+
+  return <p className="text-2xl tracking-widest pt-3 text-primary-400">{localTime}</p>
+}
--- src/components/world/worldMap.tsx
+++ src/components/world/worldMap.tsx
@@ -1,26 +1,14 @@
-import { FC, useEffect, useRef, useState } from "react"
+import { FC } from "react"
 import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps"
 
 import { Weather } from "../../types"
-import { getTimeFormatter } from "../../utilities"
+import { CityInfo } from "./cityInfo"
 import geoUrl from "./map.json"
 
 export type WorldMapProps = Pick<Weather, "city" | "country" | "countryCode" | "latitude" | "longitude"> & { showMarker: boolean }
 
 export const WorldMap: FC<WorldMapProps> = (props) => {
   const { city, country, countryCode, latitude, longitude, showMarker } = props
-
-  const intervalId = useRef<NodeJS.Timeout>()
-  const [localTime, setLocalTime] = useState(getTimeFormatter("GB", "medium").format(Date.now()))
-  const oneSecond = 1000
-
-  useEffect(() => {
-    const callback = () => setLocalTime(getTimeFormatter(countryCode || "GB", "medium").format(new Date()))
-    const cleanUp = () => clearInterval(intervalId.current)
-    intervalId.current = setInterval(callback, oneSecond)
-    return cleanUp
-  }, [countryCode])
-
   return (
     <span className="grid grid-cols-map w-full mt-4 rounded-t-lg bg-secondary-600 text-secondary-300">
       <div>
@@ -45,9 +33,11 @@ export const WorldMap: FC<WorldMapProps> = (props) => {
         </ComposableMap>
       </div>
       <span className="flex flex-col justify-center items-start pl-4">
-        <h4 className="text-sm md:text-xl lg:text-3xl">{city}</h4>
-        <p className="text-xs md:text-sm lg:text-md">{country}</p>
-        <p className="text-md tracking-widest pt-3">{localTime}</p>
+        <span className="flex flex-col justify-end min-w-[120px] min-h-[120px] px-3 py-2 rounded-lg bg-gradient-to-r from-secondary-700 to-secondary-600" >
+          <h4 className="text-sm md:text-xl lg:text-3xl">{city}</h4>
+          <p className="text-xs md:text-sm lg:text-md">{country}</p>
+          <CityInfo countryCode={countryCode}/>
+        </span>
       </span>
     </span>
   )
