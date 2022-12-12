1cffad395cb8c6572524c13c72400768bc0f39bc
debugme <debugme@hotmail.com>
2022-12-12 16:39:27 +0000

    commit 89

---
+++ src/providers/avatars/index.tsx
@@ -0,0 +1,14 @@
+import { AvatarInfo } from '../../types';
+import { Avatar1Icon } from './avatar1Icon';
+import { Avatar2Icon } from './avatar2Icon';
+import { Avatar3Icon } from './avatar3Icon';
+import { Avatar4Icon } from './avatar4Icon';
+import { Avatar5Icon } from './avatar5Icon';
+
+export const avatarInfoList: AvatarInfo[] = [
+  { id: "avatar1", avatar: <Avatar1Icon /> },
+  { id: "avatar2", avatar: <Avatar2Icon /> },
+  { id: "avatar3", avatar: <Avatar3Icon /> },
+  { id: "avatar4", avatar: <Avatar4Icon /> },
+  { id: "avatar5", avatar: <Avatar5Icon /> },
+]
--- src/providers/settings.tsx
+++ src/providers/settings.tsx
@@ -1,22 +1,10 @@
 import { createContext, FC, PropsWithChildren, useContext, useEffect, useMemo, useState } from "react"
 
-import { AvatarInfo, SettingsValue } from "../types"
+import { SettingsValue } from "../types"
 
-import { Avatar1Icon } from "./avatar1Icon"
-import { Avatar2Icon } from "./avatar2Icon"
-import { Avatar3Icon } from "./avatar3Icon"
-import { Avatar4Icon } from "./avatar4Icon"
-import { Avatar5Icon } from "./avatar5Icon"
+import { avatarInfoList } from "./avatars"
 
 const themeList = ["slate", "grey", "zinc", "plain", "stone"]
-
-const avatarInfoList: AvatarInfo[] = [
-  { id: "avatar1", avatar: <Avatar1Icon /> },
-  { id: "avatar2", avatar: <Avatar2Icon /> },
-  { id: "avatar3", avatar: <Avatar3Icon /> },
-  { id: "avatar4", avatar: <Avatar4Icon /> },
-  { id: "avatar5", avatar: <Avatar5Icon /> },
-]
 
 const initialValue = {
   showBreakpoints: false,
