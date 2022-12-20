d25419f47b1c46fa9c39e13041638ceacaa359ae
debugme <debugme@hotmail.com>
2022-12-20 09:46:31 +0000

    commit 127

--- src/components/search/field.tsx
+++ src/components/search/field.tsx
@@ -1,4 +1,5 @@
 import { ChangeEventHandler, FC } from "react";
+import { useLanguages } from "../../providers";
 
 export type FieldProps = {
   value: string;
@@ -7,13 +8,14 @@ export type FieldProps = {
 
 export const Field: FC<FieldProps> = (props) => {
   const { value, onChange } = props
+  const { t } = useLanguages()
   return (
     <input
       id="searchBox"
       className="w-full py-4 border-none rounded-lg lg:rounded-b-lg lg:rounded-t-none text-secondary-800 placeholder-secondary-600 selection:text-primary-800 selection:bg-primary-300 bg-secondary-400 pl-11 outline-secondary-400 focus:outline-white"
       type="search"
-      placeholder="Type in a city name"
-      title="Type in a city name"
+      placeholder={t("typeCity")}
+      title={t("typeCity")}
       value={value}
       onChange={onChange}
       tabIndex={0}
