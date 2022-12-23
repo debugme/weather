e32f495853ca922aafb123c16c2380de47e542cd
debugme <debugme@hotmail.com>
2022-12-23 22:45:36 +0000

    commit 142

--- src/components/world/worldMap.tsx
+++ src/components/world/worldMap.tsx
@@ -7,24 +7,27 @@ import geoUrl from "./map.json"
 
 export type WorldMapProps = Pick<Weather, "city" | "country" | "countryCode" | "latitude" | "longitude"> & { showMarker: boolean }
 
+const geographyStyle = {
+  default: { outline: "none" },
+  hover: { outline: "none" },
+  pressed: { outline: "none" },
+}
+
 export const WorldMap: FC<WorldMapProps> = (props) => {
   const { city, country, countryCode, latitude, longitude, showMarker } = props
+
   return (
     <span className="grid grid-cols-map w-full mt-4 rounded-t-lg bg-secondary-600 text-secondary-300">
       <div>
         <ComposableMap
           projectionConfig={{ scale: 140 }}
           width={800}
-          height={500}
+          height={600}
           style={{ width: "100%", height: "auto" }}
           className="fill-secondary-200">
           <Geographies geography={geoUrl}>
             {({ geographies }) =>
-              geographies.map((geo) => {
-                return (
-                  <Geography key={geo.rsmKey} geography={geo} tabIndex={-1}/>
-                )
-              })
+              geographies.map((geo) => <Geography key={geo.rsmKey} geography={geo} tabIndex={-1} style={geographyStyle} />)
             }
           </Geographies>
           {showMarker ? <Marker coordinates={[longitude, latitude]}>
@@ -36,7 +39,7 @@ export const WorldMap: FC<WorldMapProps> = (props) => {
         <span className="flex flex-col justify-end min-w-[120px] min-h-[120px] px-3 py-2 rounded-lg bg-gradient-to-r from-secondary-700 to-secondary-600" >
           <h4 className="text-sm md:text-xl lg:text-3xl capitalize">{city}</h4>
           <p className="text-xs md:text-sm lg:text-md capitalize">{country}</p>
-          <CityInfo countryCode={countryCode}/>
+          <CityInfo countryCode={countryCode} />
         </span>
       </span>
     </span>
