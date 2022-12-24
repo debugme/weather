8e950ec73aa4cb90c4b9330f6a844907cdf4bf9d
debugme <debugme@hotmail.com>
2022-12-24 20:42:13 +0000

    commit 147

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -5,7 +5,7 @@ import { Devices } from "./devices"
 export const Footer = () => {
   const { showBreakpoints } = useBreakpoints()
   return (
-    <footer className="flex items-center justify-end px-6 cursor-pointer text-secondary-700 bg-secondary-700 ">
+    <footer className="flex items-center justify-end px-6 bg-secondary-700 ">
       {showBreakpoints ? <Devices /> : null}
       {showBreakpoints ? <Breakpoints /> : null}
     </footer>
