8d51adce52c697554c645a66bf2b79a4e468da48
debugme <debugme@hotmail.com>
2022-12-28 17:40:59 +0000

    commit 155

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,41 +1,46 @@
 import { Fragment, useEffect, useMemo, useState } from "react"
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+
+
 
 import { useAvatars, useSettings } from "../../../providers"
 
 import { Menu } from "./menu"
 
 export const Header = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const location = useLocation()
+  const [pathname, setPathname] = useState(location.pathname)
   const { avatar, avatarMap } = useAvatars()
   const { handle } = useSettings()
   const navigate = useNavigate()
 
-  const icon = avatarMap[avatar]
 
-  const onClick = useMemo(() => () => setIsOpen(isOpen => !isOpen), [])
+
 
   const onEscape = () => {
     const handler = (event: KeyboardEvent) => {
       if (event.key === "Escape") {
+        console.log("ESC detected ", location.pathname);
         event.preventDefault()
-        setIsOpen(isOpen => !isOpen)
+        navigate(location.pathname === "/settings" ? "/" : "/settings")
       }
     }
     document.addEventListener("keydown", handler)
     const cleaner = () => document.removeEventListener("keydown", handler)
     return cleaner
-  }
+  }  
 
-  const onMenuToggle = () => navigate(isOpen ? "/settings" : "/")
+  
+  useEffect(() => setPathname(location.pathname.toLocaleLowerCase()), [location])
+  useEffect(onEscape, [location])
+  const toggleSettings = useMemo(() => () => navigate(pathname === "/settings" ? "/" : "/settings"), [pathname])
 
-  useEffect(onEscape, [])
-  useEffect(onMenuToggle, [isOpen])
+  const icon = avatarMap[avatar]
 
   return (
     <Fragment>
       <header className="flex items-center justify-between px-6 bg-secondary-700">
-        <Menu isOpen={isOpen} onClick={onClick} />
+        <Menu isOpen={pathname === "/settings"} onClick={toggleSettings} />
         <span className="flex items-center justify-center gap-2">
           <h3 className="mt-2 text-sm hidden sm:block text-transparent bg-clip-text bg-primary-500">{handle}</h3>
           <div className="rounded-full bg-primary-500 w-10 h-10 relative">
@@ -46,3 +51,4 @@ export const Header = () => {
     </Fragment>
   )
 }
+
