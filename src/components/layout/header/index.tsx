9181e820efb814dd78bf768be1db087b8466210c
debugme <debugme@hotmail.com>
2022-12-29 14:36:37 +0000

    commit 162

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,11 +1,15 @@
+import { useSecurity } from "../../../providers";
 import { Badge } from "./badge";
 import { Menu } from "./menu";
 
 export const Header = () => {
+  const { userProfile } = useSecurity()
+  const isSignedIn = !!userProfile
+
   return (
     <header className="flex items-center justify-between px-6 bg-secondary-700">
-      <Menu settings="/settings" shortcut="Escape" />
-      <Badge />
+      {isSignedIn ? <Menu settings="/settings" shortcut="Escape" /> : null}
+      {isSignedIn ? <Badge /> : null}
     </header>
 
   )
