6ae0355a7dbdb11301bd80249e5a872008b9b73e
debugme <debugme@hotmail.com>
2023-01-04 14:26:00 +0000

    commit 180

--- src/pages/security.tsx
+++ src/pages/security.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, useState } from "react"
+import { ChangeEventHandler, FormEvent, FormEventHandler, useState } from "react"
 import { Navigate } from "react-router-dom"
 
 import { useLocales, useSecurity } from "../providers"
@@ -16,13 +16,18 @@ export const Security = () => {
     setEmail(email)
   }
 
+  const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault()
+    signIn()
+  }
+
   if (isSignedIn)
     return <Navigate to="/" replace />
 
   return (
     <section className="flex flex-col w-3/4 mx-auto">
       <h2 className="text-3xl text-secondary-400 capitalize">{t("signIn")}</h2>
-      <form className="flex flex-col py-4 w-72 text-sm" onSubmit={signIn}>
+      <form className="flex flex-col py-4 w-72 text-sm" onSubmit={onSubmit}>
         <label htmlFor="email" className="text-secondary-400 pt-2 pb-4 text-sm">{t("typeEmail")}</label>
         <input
           className="py-3 px-4 rounded-lg"
