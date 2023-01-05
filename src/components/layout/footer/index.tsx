7a198b10b2f42f23730e2b4cf28c9946c78fde93
debugme <debugme@hotmail.com>
2023-01-05 16:35:39 +0000

    commit 195

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,14 +1,19 @@
-import { useBreakpoints } from '../../../providers'
+import { useBreakpoints, useSecurity } from '../../../providers'
 
 import { Breakpoints } from './breakpoints'
 import { Devices } from './devices'
 
 export const Footer = () => {
+	const { isSignedIn } = useSecurity()
 	const { breakpoints } = useBreakpoints()
+
+	const [breakpointInfo, deviceInfo] =
+		isSignedIn && breakpoints ? [<Breakpoints />, <Devices />] : [null, null]
+
 	return (
-		<footer className="flex items-center justify-end px-6 bg-secondary-700 ">
-			{breakpoints ? <Devices /> : null}
-			{breakpoints ? <Breakpoints /> : null}
+		<footer className="flex items-center justify-end px-6 bg-secondary-700">
+			{deviceInfo}
+			{breakpointInfo}
 		</footer>
 	)
 }
